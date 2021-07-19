ef518ab298f6cea73b70038720e20e0dc7746bd1
Manav Verma <38263736+vManav123@users.noreply.github.com>
2021-07-19 13:25:32 +0530

    Update Main.java

--- src/main/java/user/management/system/Main.java
+++ src/main/java/user/management/system/Main.java
@@ -3,13 +3,22 @@ package user.management.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import user.management.system.model.bank.Bank_Account;
 import user.management.system.model.user.Credentials;
 import user.management.system.model.user.User;
 
 @SpringBootApplication
+@RestController
 public class Main {
 
+    @GetMapping(path = "/health")
+    public String welcome()
+    {
+        return "Welcome ! , Your Code is deployed successfully on azure";
+    }
     public static void main(String[] args) { SpringApplication.run(Main.class, args); }
     @Bean
     public User getUser() {
